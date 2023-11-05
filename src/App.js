8951ad243cac2161db72a196aeb4f8afd89b8e3e
medhoucem <houcemghrairi@gmail.com>
2023-11-05 16:11:37 +0100

    map

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import SimpleMap from './MapComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleMap/>
+    </div>
+  );
+}
+
+export default App;
